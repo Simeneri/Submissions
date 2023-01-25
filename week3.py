eb6da8719058b2abc9cf6599313fb589e0986c6e
Simeneri <simenkongen@hotmail.com>
2023-01-25 15:51:39 +0100

    first commit

---
+++ week3.py
@@ -0,0 +1,48 @@
+
+#Task: Example (demonstrating how to complete a task)
+#Define the variable "puppyName" and instanciate it with the name Fido
+puppyName = "Fido" 
+
+
+#Task 1
+#Fix the following code
+puppyAge = 4
+
+
+#Task 2
+#Define a variable "breed" of type String  
+breed = ""
+
+#Task 3
+#Set the value of breed to Labrador Retriever
+breed = "Labrador Retriever"
+
+
+#Task 4
+#Fix the following code
+
+def concatinatePuppyInfo(puppyName,puppyAge,breed): 
+  return f"{puppyName}, {puppyAge}, {breed}" 
+
+
+#Task 5
+#Write a function convertDogToHumanAge that returns the human equivalent of dog years when we use the ratio 1:8 (i.e one dog year is eight human). 
+#Dog years should be given as a parameter to the function.
+
+def convertDogToHumanAge():
+  HumanAge = puppyAge*8
+  print (HumanAge)
+
+()
+convertDogToHumanAge()
+
+
+
+
+
+
+
+
+
+
+
