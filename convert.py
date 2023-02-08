222653a127232529b93adbf0d3b2d5d74981e7a3
Simeneri <simenkongen@hotmail.com>
2023-02-08 09:42:39 +0100

    https://github.com/Simeneri/submissions

---
+++ convert.py
@@ -0,0 +1,9 @@
+
+#konvertere ti talls system til romersk tall system
+
+inch_value = int(input("Enter the value in inches: "))
+cm_value = 2.54 * inch_value
+
+print ( '{}" = {}cm' .format(inch_value, cm_value))
+
+
